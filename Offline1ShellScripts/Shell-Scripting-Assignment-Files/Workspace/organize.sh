9b02436053599c3f6df0b6fb8cea8106c3e89a06
tahmid-404-20 <1905002@ugrad.cse.buet.ac.bd>
2023-06-14 17:11:23 +0600

    switches added

--- Offline1ShellScripts/Shell-Scripting-Assignment-Files/Workspace/organize.sh
+++ Offline1ShellScripts/Shell-Scripting-Assignment-Files/Workspace/organize.sh
@@ -5,41 +5,37 @@ fileType=""
 testCount=""
 
 execute() {
-    
+
     filePath="$2/$fileType/$5"
 
     c=C
     java=Java
     python=Python
-    
+
     # compile the program
     if [ $fileType == $c ]; then
         gcc "$filePath"/main.c -o "$filePath"/main.out
-        
+
     elif [ $fileType == $java ]; then
-        
+
         javac "$filePath"/Main.java
     fi
-
 
     # execute tests
     matched=0
     notMatched=0
-    for((j=1;j<=$testCount;j++))
-    do
-       file="out$j.txt"
-       touch "$filePath"/$file 
-
-       
+    for ((j = 1; j <= $testCount; j++)); do
+        file="out$j.txt"
+        touch "$filePath"/$file
 
         if [ $fileType == $c ]; then
-            "$filePath"/main.out < "$3/test$j.txt" > "$filePath"/$file
-        
+            "$filePath"/main.out <"$3/test$j.txt" >"$filePath"/$file
+
         elif [ $fileType == $java ]; then
-            java -cp "$filePath" Main < "$3/test$j.txt" > "$filePath"/$file
-        
+            java -cp "$filePath" Main <"$3/test$j.txt" >"$filePath"/$file
+
         elif [ $fileType == $python ]; then
-            python3 "$filePath"/main.py < "$3/test$j.txt" > "$filePath"/$file       
+            python3 "$filePath"/main.py <"$3/test$j.txt" >"$filePath"/$file
         fi
 
         # match the tests
@@ -50,19 +46,16 @@ execute() {
         fi
     done
 
-    echo "$5,$fileType,$matched,$notMatched" >> "$2"/result.csv
+    echo "$5,$fileType,$matched,$notMatched" >>"$2"/result.csv
 }
 
 visit() {
-    
-    if [ -d "$1" ]
-    then
-        for i in "$1"/*
-        do
+
+    if [ -d "$1" ]; then
+        for i in "$1"/*; do
             visit "$i" $2 $3
         done
-    elif [ -f "$1" ]
-    then
+    elif [ -f "$1" ]; then
         # echo "$1"
         str="$1"
         cExt=${str: -2}
@@ -72,7 +65,6 @@ visit() {
         c=.c
         java=.java
         python=.py
-
 
         if [ $cExt == $c ]; then
             mkdir "$3"/C/$2
@@ -88,92 +80,116 @@ visit() {
             fileType="Python"
         fi
 
-    fi     
+    fi
 }
 
+if [ $# -lt 4 ]; then
+    echo "Usage:"
+    echo "./organize.sh <submission folder> <target folder> <test folder> <answer folder> [-v] [-noexecute]"
+    echo ""
+    echo "-v: verbose"
+    echo "-noexecute: do not execute code files"
 
-# remove the targets folder, create directories and subdirectories
-rm -rf $2
-mkdir  $2
-mkdir $2/C
-mkdir $2/Java
-mkdir $2/Python
+else
+    # remove the targets folder, create directories and subdirectories
+    rm -rf $2
+    mkdir $2
+    mkdir $2/C
+    mkdir $2/Java
+    mkdir $2/Python
 
-uzip="unzipped"
-rm -rf "$1"/$uzip
+    uzip="unzipped"
+    rm -rf "$1"/$uzip
 
-testCount=`ls "$3" | wc -l`
+    # checking the switches
+    verbose=0
+    isExecuted=1
+    for i in $*; do
+        if [ $i == "-v" ]; then
+            verbose=1
+        elif [ $i == "-noexecute" ]; then
+            isExecuted=0
+        fi
+    done
 
-# go through the files in submissions folder
+    testCount=$(ls "$3" | wc -l)
 
-touch "$2"/result.csv
-echo "student_id,type,matched,not_matched" >> "$2"/result.csv
-
-for i in "$1"/* 
-do
-    if [ -f "$i" ]; then
-        fileName="$i"
-        
-        stdId=${fileName%.zip}
-        stdId=${stdId: -7}
-        # echo $stdId
-
-        mkdir "$1"/$uzip
-        unzip -qq "$i" -d "$1"/$uzip
-
-        echo "Organizing file for $stdId"
-        visit "$1"/$uzip $stdId $2
-        rm -r "$1"/$uzip
-
-        echo "Executing file for $stdId"
-        execute "$1" "$2" "$3" "$4" $stdId        
+    if [ $isExecuted == 1 ]; then
+        touch "$2"/result.csv
+        echo "student_id,type,matched,not_matched" >>"$2"/result.csv
     fi
-done
+
+    # go through the files in submissions folder
+    for i in "$1"/*; do
+        if [ -f "$i" ]; then
+            fileName="$i"
+
+            stdId=${fileName%.zip}
+            stdId=${stdId: -7}
+            # echo $stdId
+
+            mkdir "$1"/$uzip
+            unzip -qq "$i" -d "$1"/$uzip
+
+            if [ $verbose == 1 ]; then
+                echo "Organizing files of $stdId"
+            fi
+
+            visit "$1"/$uzip $stdId $2
+            rm -r "$1"/$uzip
+
+            if [ $isExecuted == 1 ]; then
+                if [ $verbose == 1 ]; then
+                    echo "Executing files of $stdId"
+                fi
+                execute "$1" "$2" "$3" "$4" $stdId
+            fi
+        fi
+    done
+
+fi
 
 # Now compiling and executing the programs
 
-
 # # C program
 # for i in "$2"/C/*
-# do  
+# do
 #     gcc "$i"/main.c -o "$i"/main.out
 
 #     # echo ls "$3/" | wc -l
 #     for((j=1;j<=$testCount;j++))
 #     do
 #        file="out$j.txt"
-#        touch "$i"/$file 
+#        touch "$i"/$file
 #        "$i"/main.out < "$3/test$j.txt" > "$i"/$file
 
 #     #    echo "$i"/main "$3/test$j.txt" "$i"/$file
 #     done
 # done
 
-
 # # Java
 # for i in "$2"/Java/*
-# do  
+# do
 #     javac "$i"/Main.java
 
 #     for((j=1;j<=$testCount;j++))
 #     do
 #        file="out$j.txt"
-#        touch "$i"/$file 
+#        touch "$i"/$file
 #        java -cp "$i" Main < "$3/test$j.txt" > "$i"/$file
 
 #     #    echo "$i"/Main "$3/test$j.txt" "$i"/$file
 #     done
 # done
 
-
 # # Python
 # for i in "$2"/Python/*
-# do  
+# do
 
 #     for((j=1;j<=$testCount;j++))
 #     do
 #        file="out$j.txt"
-#        touch "$i"/$file 
+#        touch "$i"/$file
 #        python3 "$i"/main.py < "$3/test$j.txt" > "$i"/$file
 
 #     #    echo "$i"/Main "$3/test$j.txt" "$i"/$file
